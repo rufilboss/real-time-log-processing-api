f0f8a820e58996175651b2ac06fbbb0ca52e80d1
Ilyas Rufai <rufaiilyas@gmail.com>
2024-12-09 06:28:53 +0100

    feat: add IAC, CI/CD pipeline and Kubernetes manifests

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "app_name" {
+  description = "Name of the application"
+  default     = "log-processing-api"
+}
